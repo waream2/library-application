91d59fa0671bd3d6ca9f8b8c270f83bd88439dcc
earn <waream2@gmail.com>
2021-11-07 21:40:51 -0500

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://waream2:zxcxz@cluster0.njpli.mongodb.net/test';
+var dev_db_url = 'mongodb+srv://waream2:zxcxz@cluster0.njpli.mongodb.net/test'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
